7cd91275feada183290316c75516207fe2491a44
Mohamed Mohamud <mohamedmohamud@Mohameds-MacBook-Pro.local>
2022-05-31 21:35:59 -0400

    1st commit

---
+++ src/gitReview/gitReview.java
@@ -0,0 +1,11 @@
+package gitReview;
+
+public class gitReview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("today we learn github");
+		System.out.print("today is a very good day"); 
+	}
+
+}
